2fdc3218eaf391667f2d5ebf605e109c3f1705dc
Daniel Joseph Adoremus <djadoremus@gmail.com>
2013-06-21 08:21:14 +0800

    removed android studio specific files

--- SmartInfinityLifestyle/build/source/buildConfig/debug/hk/com/novare/smart/infinitylifestyle/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package hk.com.novare.smart.infinitylifestyle;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
